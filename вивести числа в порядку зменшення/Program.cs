3b6ded55217a90cf873a6dfe5b18043764836d02
dshaposhnik000@gmail.com <dshaposhnik000@gmail.com>
2023-05-09 19:37:45 +0300

    Добавьте файлы проекта.

---
+++ вивести числа в порядку зменшення/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вивести_числа_в_порядку_зменшення
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            int num2;
+            int i;
+            do
+            {
+                Console.WriteLine("Enter the number 1: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number 2: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+            } while (num1 < num2);
+            
+            do
+            {
+                for (i = num1; num1 >= num2; num1--)
+                {
+                    Console.WriteLine("{0}", num1);
+                }
+            } while (i != num2);
+
+           
+
+            
+
+        }
+    }
+}
